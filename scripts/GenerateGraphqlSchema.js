920687d87690a8a8406e68d3c53dda807e5c8d81
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-12-23 10:30:16 +1300

    Packages upgraded

--- scripts/GenerateGraphqlSchema.js
+++ scripts/GenerateGraphqlSchema.js
@@ -4,10 +4,10 @@ import { graphql, printSchema } from 'graphql';
 import { introspectionQuery } from 'graphql/utilities';
 import { getRootSchema } from '../src';
 
-fs.writeFileSync(path.resolve(__dirname, '../data/schema.graphql'), printSchema(getRootSchema()));
+fs.writeFileSync(path.resolve(__dirname, '../data/schema.graphql'), printSchema(getRootSchema(), { commentDescriptions: true }));
 
 graphql(getRootSchema(), introspectionQuery)
-  .then(json => {
+  .then((json) => {
     fs.writeFileSync(path.resolve(__dirname, '../data/schema.json'), JSON.stringify(json, null, 2));
     console.log('Done');
   })
